41ad1ce78b0b18d5703fca6122b4082182b2d85d
DoodleBears <yangmufeng233@gmail.com>
2020-05-30 00:43:08 +0800

    Lesson-6 Camera

--- assets/script/game.js
+++ assets/script/game.js
@@ -3,7 +3,10 @@ cc.Class({
 
     properties: {
         // Binding tiledMap(the property) to cc.TiledMap(allow us to add file in Cocos Creator)
-        tiledMap: cc.TiledMap,
+        //tiledMap: cc.TiledMap,
+        
+        //mapNode to read the node in Cocos as map
+        mapNode: cc.Node,
         // allow game.js to access the "dialog" node
         dialogNode: cc.Node,
     },
@@ -12,37 +15,41 @@ cc.Class({
         let p = cc.director.getPhysicsManager();
         p.enabled = true;
         // Draw collider space 绘制碰撞区域
-        // p.debugDrawFlags = true;
+        //p.debugDrawFlags = true;
         p.gravity = cc.v2(0, 0);
         
     },
 
     start () { 
         // get data from TiledMap, tiledMap is what we select in Cocos Creator
-        let tiledSize = this.tiledMap.getTileSize();
-        let layer = this.tiledMap.getLayer('wall');
-        let layerSize = layer.getLayerSize();
+        for (let mapNode of this.mapNode.children) {
 
-        for (let i = 0; i < layerSize.width; i++){
-            for (let j = 0; j < layerSize.height; j++){
-                // set every tiled's arttribute
-                let tiled = layer.getTiledTileAt(i, j, true);
-                //check if there exists a block in map
-                if (tiled.gid != 0){
-                    // set tiled's group in order to make collision
-                    tiled.node.group = 'wall';
+            let tiledMap = mapNode.getComponent(cc.TiledMap);
+            let tiledSize = tiledMap.getTileSize();
+            let layer = tiledMap.getLayer('wall');
+            let layerSize = layer.getLayerSize();
 
-                    // add an component to each tiled and bind it with "body"
-                    let body = tiled.node.addComponent(cc.RigidBody);
-                    //use body. to set RigidBody's arttribute
-                    body.type = cc.RigidBodyType.Static;
-                    //PhysicsBoxCollider's defult anthor is center point
-                    let collider =tiled.node.addComponent(cc.PhysicsBoxCollider);
-                    //since the anchor in map we set is left-down, we add offset to collider
-                    collider.offset = cc.v2(tiledSize.width / 2 ,tiledSize.height / 2);
-                    collider.size = tiledSize;
-                    // we need to apply the collider
-                    collider.apply();
+            for (let i = 0; i < layerSize.width; i++){
+                for (let j = 0; j < layerSize.height; j++){
+                    // set every tiled's arttribute
+                    let tiled = layer.getTiledTileAt(i, j, true);
+                    //check if there exists a block in map
+                    if (tiled.gid != 0){
+                        // set tiled's group in order to make collision
+                        tiled.node.group = 'wall';
+
+                        // add an component to each tiled and bind it with "body"
+                        let body = tiled.node.addComponent(cc.RigidBody);
+                        //use body. to set RigidBody's arttribute
+                        body.type = cc.RigidBodyType.Static;
+                        //PhysicsBoxCollider's defult anthor is center point
+                        let collider =tiled.node.addComponent(cc.PhysicsBoxCollider);
+                        //since the anchor in map we set is left-down, we add offset to collider
+                        collider.offset = cc.v2(tiledSize.width / 2 ,tiledSize.height / 2);
+                        collider.size = tiledSize;
+                        // we need to apply the collider
+                        collider.apply();
+                    }
                 }
             }
         }
